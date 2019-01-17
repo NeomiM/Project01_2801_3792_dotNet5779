1bb8927a1dc4f21b4d66de4897dfad9eafe9b3ad
משתמש <@DESKTOP-WELL>
2019-01-17 23:15:24 +0200

    update test

--- PLWPF/UpdateTest.xaml.cs
+++ PLWPF/UpdateTest.xaml.cs
@@ -62,7 +62,7 @@ namespace PLWPF
             u_testerIdTextBox.IsEnabled = false;
             u_traineeIdTextBox.IsEnabled = false;
             u_commentsGroupBox.IsEnabled = false;
-            testPassedRadioButton.IsEnabled = false;
+            testPassedCheckBox.IsEnabled = false;
             BoolItemsGrid.IsEnabled = false;
             saveButton.IsEnabled = false;
         }
@@ -72,7 +72,7 @@ namespace PLWPF
             u_testerIdTextBox.IsEnabled = true;
             u_traineeIdTextBox.IsEnabled = true;
             u_commentsGroupBox.IsEnabled = true;
-            testPassedRadioButton.IsEnabled = true;
+            testPassedCheckBox.IsEnabled = true;
             BoolItemsGrid.IsEnabled = true;
             saveButton.IsEnabled = true;
         }
@@ -80,16 +80,17 @@ namespace PLWPF
         public bool ThereNoEmptyFiles()
         {
             bool allfill = true;
-            if (checkMirrorsCheckBox.IsChecked == false || checkMirrorsCheckBox.IsChecked == null) allfill = false;
-            if (imediateStopCheckBox.IsChecked == false || imediateStopCheckBox.IsChecked == null) allfill = false;
-            if (keptDistanceCheckBox.IsChecked == false || keptDistanceCheckBox.IsChecked == null) allfill = false;
-            if (keptRightofPresidenceCheckBox.IsChecked == false || keptRightofPresidenceCheckBox.IsChecked == null) allfill = false;
-            if (parkingCheckBox.IsChecked == false || parkingCheckBox.IsChecked == null) allfill = false;
-            if (reverseParkingCheckBox.IsChecked == false || reverseParkingCheckBox.IsChecked == null) allfill = false;
-            if (rightTurnCheckBox.IsChecked == false || rightTurnCheckBox.IsChecked == null) allfill = false;
-            if (stoppedAtcrossWalkCheckBox.IsChecked == false || stoppedAtcrossWalkCheckBox.IsChecked == null) allfill = false;
-            if (stoppedAtRedCheckBox.IsChecked == false || stoppedAtRedCheckBox.IsChecked == null) allfill = false;
-            if (usedSignalCheckBox.IsChecked == false || usedSignalCheckBox.IsChecked == null) allfill = false;
+            
+            if (checkMirrorsCheckBox.IsChecked == false || checkMirrorsCheckBox.IsThreeState == false) allfill = false;
+            if (imediateStopCheckBox.IsChecked == false || imediateStopCheckBox.IsThreeState == false) allfill = false;
+            if (keptDistanceCheckBox.IsChecked == false || keptDistanceCheckBox.IsThreeState == false) allfill = false;
+            if (keptRightofPresidenceCheckBox.IsChecked == false || keptRightofPresidenceCheckBox.IsThreeState == false) allfill = false;
+            if (parkingCheckBox.IsChecked == false || parkingCheckBox.IsThreeState == false) allfill = false;
+            if (reverseParkingCheckBox.IsChecked == false || reverseParkingCheckBox.IsThreeState == false) allfill = false;
+            if (rightTurnCheckBox.IsChecked == false || rightTurnCheckBox.IsThreeState == false) allfill = false;
+            if (stoppedAtcrossWalkCheckBox.IsChecked == false || stoppedAtcrossWalkCheckBox.IsThreeState == false) allfill = false;
+            if (stoppedAtRedCheckBox.IsChecked == false || stoppedAtRedCheckBox.IsThreeState == false) allfill = false;
+            if (usedSignalCheckBox.IsChecked == false || usedSignalCheckBox.IsThreeState == false) allfill = false;
             return allfill;
         }
         #endregion
@@ -105,11 +106,6 @@ namespace PLWPF
             System.Windows.Data.CollectionViewSource testViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("testViewSource")));
             // Load data by setting the CollectionViewSource.Source property:
             // testViewSource.Source = [generic data source]
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            //this.Close();
         }
 
         private void TestIdComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -128,9 +124,74 @@ namespace PLWPF
 
         private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
-            if (!ThereNoEmptyFiles())
-                warningTextBlock.Visibility = Visibility.Visible;
+            //if(saveButton.Content == "Check")
+            //{
+                if (!ThereNoEmptyFiles())
+                    warningTextBlock.Visibility = Visibility.Visible;
+                if (testPassedCheckBox.IsChecked == false || testPassedCheckBox.IsThreeState == false)
+                    warningTextBlock1.Visibility = Visibility.Visible;
+                if (warningTextBlock.Visibility == Visibility.Hidden && warningTextBlock1.Visibility == Visibility.Hidden)
+            {
+                //saveButton.Content = "Save";
+                Button.IsEnabled = true;
+            }
+            //}
+            //if(saveButton.Content == "Save")
+            //{
+            //    bl.UpdateTest(TestForPL);
+            //    MessageBox.Show("Test saved successfully", "", MessageBoxButton.OK, MessageBoxImage.Information);
+            //}
         }
-    
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (keptDistanceCheckBox.IsChecked == true)
+                TestForPL.KeptDistance = true;
+            else TestForPL.KeptDistance = false;
+
+            if (parkingCheckBox.IsChecked == true)
+                TestForPL.Parking = true;
+            else TestForPL.Parking = false;
+
+            if (reverseParkingCheckBox.IsChecked == true)
+                TestForPL.ReverseParking = true;
+            else TestForPL.ReverseParking = false;
+
+            if (checkMirrorsCheckBox.IsChecked == true)
+                TestForPL.CheckMirrors = true;
+            else TestForPL.CheckMirrors = false;
+
+            if (usedSignalCheckBox.IsChecked == true)
+                TestForPL.UsedSignal = true;
+            else TestForPL.UsedSignal = false;
+
+            if (keptRightofPresidenceCheckBox.IsChecked == true)
+                TestForPL.KeptRightofPresidence = true;
+            else TestForPL.KeptRightofPresidence = false;
+
+            if (stoppedAtRedCheckBox.IsChecked == true)
+                TestForPL.StoppedAtRed = true;
+            else TestForPL.StoppedAtRed = false;
+
+            if (stoppedAtcrossWalkCheckBox.IsChecked == true)
+                TestForPL.StoppedAtcrossWalk = true;
+            else TestForPL.StoppedAtcrossWalk = false;
+
+            if (rightTurnCheckBox.IsChecked == true)
+                TestForPL.RightTurn = true;
+            else TestForPL.RightTurn = false;
+
+            if (imediateStopCheckBox.IsChecked == true)
+                TestForPL.ImediateStop = true;
+            else TestForPL.ImediateStop = false;
+
+            if (testPassedCheckBox.IsChecked == true)
+                TestForPL.TestPassed = true;
+            else TestForPL.TestPassed = false;
+
+            bl.UpdateTest(TestForPL);
+
+            MessageBox.Show("Test saved successfully", "", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
     }
 }

416125d5e12eb24ade5732b1343d561ca1300b91
mayer.neomi <mayer.neomi@gmail.com>
2019-01-22 11:51:08 +0200

    dsfjoidjf

--- PLWPF/AddTestUserControl.xaml.cs
+++ PLWPF/AddTestUserControl.xaml.cs
@@ -476,10 +476,22 @@ namespace PLWPF
             //    TesterErrors.Foreground = Brushes.Orange;
             //    TesterErrors.Visibility = Visibility.Visible;
             //}
-
+            if (fileredDistance.Count != 0)
+            {
             TesterComboBox.ItemsSource = fileredDistance;
             TesterComboBox.IsEnabled = true;
             TesterComboBox.Visibility = Visibility.Visible;
+                TesterErrors.Text = "";
+                TesterErrors.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                TesterErrors.Text = "ERROR. No testers found.";
+                TesterErrors.Foreground = Brushes.Red;
+                TesterErrors.Visibility = Visibility.Visible;
+
+
+            }
             findTesters.Content = "Find Testers";
         }
 
@@ -487,6 +499,7 @@ namespace PLWPF
         {
             try
             {
+                TesterComboBox.Visibility = Visibility.Hidden;
                 TestAddressErrors.Visibility = Visibility.Collapsed;
                 checkAddress();
                      //   if (testAddress.Text==""||testAddress.Text==null)
@@ -511,18 +524,40 @@ namespace PLWPF
 
         void checkAddress()
         {
-            try
-            {
+            //try
+           // {
                 bl.IsText(city.Text);
                 bl.IsText(street.Text);
                 bl.IsNumber(stNumber.Text);
                 AddTestForPL.StartingPoint=new Address(street.Text,stNumber.Text,city.Text);
+            //}
+            //catch (Exception ex)
+            //{
+            //    TestAddressErrors.Text = ex.Message;
+            //    TestAddressErrors.Foreground = Brushes.Red;
+            //    TestAddressErrors.Visibility = Visibility.Visible;
+            //}
+        }
+
+        private void checkErrors()
+        {
+            if(AddDateErrors.Text!="" && AddDateErrors.Text != null)
+                throw new Exception();
+            if (TesterErrors.Text != "" && TesterErrors.Text != null)
+                throw new Exception();
+            if (HoursErrors.Text != "" && HoursErrors.Text != null)
+                throw new Exception();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                checkErrors();
             }
             catch (Exception ex)
             {
-                TestAddressErrors.Text = ex.Message;
-                TestAddressErrors.Foreground = Brushes.Red;
-                TestAddressErrors.Visibility = Visibility.Visible;
+                MessageBox.Show("Can't add test. Check Errors.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
